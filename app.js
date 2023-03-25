58c2246b45e99bd30340cde46df02cada76e3ce2
rahul631998 <rahulchaudhary631998@gmail.com>
2023-03-25 16:35:47 +0530

    registering user

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const ejs = require("ejs");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public")); 
+app.set('view engine', 'ejs');
+
+// const dbUsername = process.env.DB_USERNAME;
+// const dbPassword = process.env.DB_PASSWORD;
+// const url = "mongodb+srv://"+dbUsername+":"+dbPassword+"@cluster0.htya6dh.mongodb.net";
+const url = "mongodb://127.0.0.1:27017"
+const dbName = "userDB";
+
+mongoose.connect(url+'/'+dbName);
+
+const userSchema = {
+    email: String,
+    password: String
+};
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+app.get("/register", (req, res) => {
+    res.render("register");
+});
+
+app.post("/register", (req, res) => {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save()
+        .then(() => {
+            res.render("secrets")
+        })
+        .catch((err) => {
+            res.send(err);
+        });
+});
+
+app.listen(3000, ()=> {
+    console.log("Server started on port 3000");
+});
