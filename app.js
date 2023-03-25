3a9b0b051b03167b8a330def9a66db9f9c53313a
rahul631998 <rahulchaudhary631998@gmail.com>
2023-03-26 00:45:28 +0530

    using bcrypt

--- app.js
+++ app.js
@@ -7,24 +7,29 @@ const ejs = require("ejs");
 // will use md5 hashing instead of encryption
 //const encrypt = require("mongoose-encryption")
 
-const md5 = require("md5");
+// will use bcrypt hashing instead of md5
+//const md5 = require("md5");
+
+const bcrypt = require("bcrypt");
+
+const saltRounds = 10;
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public")); 
-app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
 const dbUsername = process.env.DB_USERNAME;
 const dbPassword = process.env.DB_PASSWORD;
 const url = process.env.DB_URL || "127.0.0.1:27017";
 const dbName = process.env.DB_NAME;
 
-mongoose.connect(url+'/'+dbName);
+mongoose.connect(url + "/" + dbName);
 
-const userSchema = new mongoose.Schema ({
+const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
 });
 
 const secret = process.env.SECRET;
@@ -47,38 +52,43 @@ app.get("/register", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    });
-
-    newUser.save()
-        .then(() => {
-            res.render("secrets")
-        })
-        .catch((err) => {
-            res.send(err);
+    bcrypt.hash(req.body.password, saltRounds).then((hash) => {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash,
         });
+
+        newUser
+            .save()
+            .then(() => {
+                res.render("secrets");
+            })
+            .catch((err) => {
+                res.send(err);
+            });
+    });
 });
 
 app.post("/login", (req, res) => {
     const username = req.body.username;
-    const password = md5(req.body.password;
+    const password = req.body.password;
 
-    User.findOne({email: username})
+    User.findOne({ email: username })
         .then((foundUser) => {
-            if (foundUser.password === password) {
-                res.render("secrets")
-            } else {
-                res.redirect("/");
-            }
+            bcrypt.compare(password, foundUser.password).then((result) => {
+                if (result === true) {
+                    res.render("secrets");
+                } else {
+                    res.redirect("/");
+                }
+            });
         })
         .catch((err) => {
             res.send(err);
         });
 });
 
-const port = process.env.PORT || 3000
-app.listen(port, ()=> {
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
     console.log("Server started on", port);
 });
